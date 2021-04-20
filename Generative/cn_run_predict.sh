875a62a2596bd00c033c2bbf4fc211dad309de2f
jeremysun1224 <2113751293@qq.com>
2021-04-20 09:30:52 +0800

    generative

---
+++ Generative/cn_run_predict.sh
@@ -0,0 +1,23 @@
+MODEL_PATH=E:\\Internship\\ConvQA\\GenerativeCoQA\\Experiments\\QAQAFQ_CN\\ckpt-5105
+export SPLIT=dev
+export CUDA_VISIBLE_DEVICES=0
+export OMP_NUM_THREADS=4
+export MKL_NUM_THREADS=4
+# run decoding
+python decode_seq2seq.py  --model_type unilm \
+                          --tokenizer_name C:/pretrained_model/unilm-cn-base \
+                          --input_file "ConvQA/QAQAFQ/ConvQA_dev.json" \
+                          --split $SPLIT \
+                          --do_lower_case \
+                          --model_path ${MODEL_PATH} \
+                          --max_seq_length 512 \
+                          --max_tgt_length 42 \
+                          --batch_size 96 \
+                          --mode s2s \
+                          --output_file "ConvQA/CoQA_json/mlm_qaqafq.json" \
+                          --do_rule \
+                          # --beam_size 3 \
+                          # --length_penalty 0 \
+                          # --forbid_duplicate_ngrams \
+                          # --forbid_ignore_word "." \
+                          # --fp16 --amp \
